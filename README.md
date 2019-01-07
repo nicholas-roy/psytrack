f8d9b5ca40a7aa930dbd5ae9f7d0ce80182155b0
Nicholas Roy <nchlsry42@gmail.com>
2019-01-07 07:38:41 -0500

    Basic parts of new package added: MIT License, README, setup

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# psytrack
+# PsyTrack
+
+Package under construction, please check back later!
+
+## Documentation
+
+See ... % add link
+
+
+## Examples
+
+Example here ... % add link
+
+
+## How to install
+
+Just run `pip install psytrack`
+
+
+## Authors
+
+Nick Roy, Ji Hyun Bak, and Jonathan Pillow % link to webpages
+
+Please cite as...
+
+
+% readme template from https://github.com/HIPS/autograd
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name='psytrack',
+	version='0.1',
+	description='Tool for tracking dynamic psychometric curves',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url='http://github.com/nicholas-roy/psytrack',
+	author='Nicholas A. Roy, Ji Hyun Bak, and Jonathan W. Pillow',
+	author_email='nicholas.roy.42@gmail.com',
+	license='MIT',
+    packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Intended Audience :: Science/Research",
+		"Topic :: Scientific/Engineering",
+		],
+	zip_safe=False)
